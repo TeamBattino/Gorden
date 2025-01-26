17f8d155d51ce12200fdf7dde4613a30cebaf095
Battino Loew <60046498+TeamBattino@users.noreply.github.com>
2025-01-26 16:47:37 +0000

    Abstract Services to Interfaces and Added Websocket Support

---
+++ server/src/interfaces.ts
@@ -0,0 +1,13 @@
+export interface Creatable<CreationData = any, ReturnType = any> {
+  create(data?: CreationData): Promise<ReturnType>;
+}
+
+export interface Readable<T, ID> {
+  getById(id: ID): Promise<T | undefined>;
+}
+
+export interface MessageSendingService<MessageType = any> {
+  sendMessage(message: MessageType): Promise<any>;
+  getRoomMessages(roomId: string): Promise<MessageType[]>;
+  getRoomUpdates(roomId: string, lastMessageId: number): Promise<MessageType[]>;
+}
--- server/src/services/roomService.ts
+++ server/src/services/roomService.ts
@@ -1,15 +1,24 @@
-import { BaseService } from "./baseService";
+import prisma from "@lib/database";
 import { faker } from "@faker-js/faker";
+import { BaseService } from "./baseService";
+import { Creatable, Readable } from "../interfaces";
 
-export class RoomService extends BaseService {
-  async createRoom() {
+export class RoomService
+  extends BaseService
+  implements Creatable, Readable<any, string>
+{
+  async create() {
     const room = await this.prisma.room.create({
       data: {
-        id: faker.string.alpha({ length: 6, casing: 'lower' }),
+        id: faker.string.alpha({ length: 6, casing: "lower" }),
         name: faker.color.human() + " " + faker.animal.type(),
       },
     });
     return room;
+  }
+
+  async getById(roomId: string): Promise<any | undefined> {
+    return this.getRoom(roomId);
   }
 
   async getRoom(roomId: string) {
